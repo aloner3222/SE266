18a1d26b2ecaf8ccbb9f9cebc131cf8ee3173ac0
jhhusnander <jhhusnander@email.neit.edu>
2015-02-11 10:19:47 -0500

    submit ex_08

--- ex_starts/ch07_ex1/display_results.php
+++ ex_starts/ch07_ex1/display_results.php
@@ -1,14 +1,36 @@
 <?php
     // get the data from the form
     $email = $_POST['email'];
-
+    $pass_word = $_POST['password'];
+	$phone_num = $_POST['phone'];
+	$action = $_POST['action'];
     // get the rest of the data for the form
 
     // for the heard_from radio buttons,
-    // display a value of 'Unknown' if the user doesn't select a radio button
+	if(isset($_POST['heard_from'])){
+		$heard_from = $_POST['heard_from'];
+		// display a value of 'Unknown' if the user doesn't select a radio button
+	}else{
+		$heard_from = "Unknown";
+	}
 
-    // for the wants_updates check box,
+	 // for the wants_updates check box,
+	
     // display a value of 'Yes' or 'No'
+	if(isset($_POST['wants_updates'])){
+		$wants_updates = "Yes";
+		}
+	else{
+		 $wants_updates = "No"; 
+	}
+
+    // display chosen drop down list items
+	$methods = $_POST['contact_via'];
+
+	// display comments
+	$comments = $_POST['comments'];
+	
+		
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -25,22 +47,22 @@
         <span><?php echo htmlspecialchars($email); ?></span><br />
 
         <label>Password:</label>
-        <span><!-- add PHP code here--></span><br />
+        <span><?php echo  htmlspecialchars($pass_word); ?></span><br />
 
         <label>Phone Number:</label>
-        <span></span><br />
+        <span><?php echo  htmlspecialchars($phone_num); ?></span><br />
 
         <label>Heard From:</label>
-        <span></span><br />
+        <span><?php echo $heard_from; ?></span><br />
 
         <label>Send Updates:</label>
-        <span></span><br />
+        <span><?php echo $wants_updates; ?></span><br />
 
         <label>Contact Via:</label>
-        <span></span><br /><br />
+        <span><?php echo $methods; ?></span><br /><br />
 
-        <span>Comments:</span><br />
-        <span></span><br />
+        <span>Comments:    </span>
+        <span><?php echo $comments; ?></span><br />
         
         <p>&nbsp;</p>
     </div>
--- ex_starts/ch08_ex1/index.php
+++ ex_starts/ch08_ex1/index.php
@@ -1,24 +1,57 @@
 <?php
 
-$customer_type = $_POST['type'];
+$type = $_POST['type'];
 $invoice_subtotal = $_POST['subtotal'];
 
-if ($customer_type == 'R') {
-    if ($invoice_subtotal < 100)
-        $discount_percent = .0;
-    else if ($invoice_subtotal >= 100 && $invoice_subtotal < 250)
-        $discount_percent = .1;
-    else if ($invoice_subtotal >= 250)
-        $discount_percent = .25;
-} else if ($customer_type == 'C') {
-    if ($invoice_subtotal < 250)
+$customer_type = strtoupper($type);
+switch ($customer_type) {
+    case 'R':
+        if ($invoice_subtotal < 100) {
+            $discount_percent = .0;
+        } else if ($invoice_subtotal >= 100 && $invoice_subtotal < 250) {
+            $discount_percent = .1;
+        } else if ($invoice_subtotal >= 250 && $invoice_subtotal < 500) {
+            $discount_percent = .25;
+        } else if ($invoice_subtotal >= 500) {
+            $discount_percent = .3;
+        }
+        break;
+    case 'C':
         $discount_percent = .2;
-    else
+        break;
+    case 'T':
+        if ($invoice_subtotal < 500) {
+            $discount_percent = .4;
+        } else if ($invoice_subtotal >= 500) {
+            $discount_percent = .5;
+        }
+        break;
+    default:
+        $discount_percent = .1;
+        break;
+}	
+
+/*
+if ($customer_type == 'R' || $customer_type == 'r') {
+    if ($invoice_subtotal <= 100)
+        $discount_percent = .0;
+    else if ($invoice_subtotal >= 250 && $invoice_subtotal < 500)
+        $discount_percent = .25;
+    else if ($invoice_subtotal >= 500)
         $discount_percent = .3;
+} else if ($customer_type == 'C' || $customer_type == 'c') {
+    if ($invoice_subtotal > 0)
+        $discount_percent = .2;
+}else if ($customer_type == 'T' || $customer_type == 't') { 
+	if ($invoice_subtotal < 500)
+        $discount_percent = .4;
+	else if ($invoice_subtotal >= 500)
+        $discount_percent = .5;
 } else {
-    $discount_percent = .0;
+    $discount_percent = .10;
 }
 
+*/
 $discount_amount = $invoice_subtotal * $discount_percent;
 $invoice_total = $invoice_subtotal - $discount_amount;
 
